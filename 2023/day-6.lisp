d6ac5d7962c9aea58646a235c84fec5a77509bff
Will Osborne <willfosborne@gmail.com>
2023-12-08 17:02:12 +0000

    day 6

--- 2023/day-6.lisp
+++ 2023/day-6.lisp
@@ -33,12 +33,6 @@ Distance:  9  40  200")
   (is equalp '((7 9) (15 40) (30 200)) (parse-input *sample-input*)))
 
 
-(defun number-ways-to-win (race-spec)
-  (destructuring-bind (time record) race-spec
-    (let ((solns (solutions -1 time (- record))))
-      (print solns)
-      (1+ (- (floor (- (apply #'max solns) 0.001)) (ceiling (+ 0.001 (apply #'min solns))))))
-    ))
 
 ;; distance(wait, time) = (time - wait) * wait
 ;; winning if d(w, t) > record
@@ -51,17 +45,59 @@ Distance:  9  40  200")
 ;; (-b +- sqrt(b^2 - 4ac)) / 2a
 ;;
 
-(defun solutions (a b c)
-  (format t "~a ~a ~a" a b c)
-  (when (and (<= (* 4 a c) (* b b))
-             (/= a 0))
-    (let* ((sq (sqrt (- (* b b) (* 4 a c)))))
-      (print sq)
-      (list (/ (+ (- b) sq) (* 2 a))
-            (/ (- (- b) sq) (* 2 a))))))
+;; this worked but i entered it wrong and ended up completely re-solving with a brute force.
+;; painful
 
-(define-test "solves quadratics"
+;; (defun solutions (a b c)
+;;   (format t "~a ~a ~a" a b c)
+;;   (when (and (<= (* 4 a c) (* b b))
+;;              (/= a 0))
+;;     (let* ((sq (sqrt (- (* b b) (* 4 a c)))))
+;;       (print sq)
+;;       (list (floor (/ (+ (- b) sq) (* 2 a)))
+;;             (ceiling (/ (- (- b) sq) (* 2 a)))))))
+
+;; (define-test "solves quadratics"
+;;   :parent day-6
+;;   (is equalp '(1 -5) (solutions 2 8 -10))
+;;   (false (solutions 100 2 10))
+;;   (false (solutions 0 1 1)))
+
+;; (defun number-ways-to-win (race-spec)
+;;   (destructuring-bind (time record) race-spec
+;;     (let ((solns (solutions -1 time (- -1 record ))))
+;;       (print solns)
+;;       ;; (1+ (- (floor (+ -0.001 (apply #'max solns)))
+;;       ;;         (ceiling (+ 0.001 (apply #'min solns)))))
+;;       (1+ (abs (- (car solns) (cadr solns))))
+
+;;       )
+;;     ))
+
+(defun beats-record? (delay time record)
+  (> (* delay (- time delay))
+     record))
+
+
+
+(defun race (race-spec)
+  (let* ((time (car race-spec))
+         (record (cadr race-spec))
+         (lower (iter (for i upfrom 0)
+                  (finding i such-that (beats-record? i time record))))
+         (upper (iter (for i downfrom time)
+                  (finding i such-that (beats-record? i time record)))))
+    (1+ (- upper lower))))
+
+(define-test "counts ways to win"
   :parent day-6
-  (is equalp '(1 -5) (solutions 2 8 -10))
-  (false (solutions 100 2 10))
-  (false (solutions 0 1 1)))
+  (is = 4 (race '(7 9)))
+  (is = 8 (race '(15 40)))
+  (is = 9 (race '(30 200)))
+  )
+
+(defun solution-1 (&optional (input *input*))
+  (reduce #'* (mapcar #'race (parse-input input))))
+
+(defun solution-2 ()
+  (race '(61709066 643118413621041)))
